789f493b87a41ac27a8340960b4275a37844a1af
Andre Kanayama <andre.spanguero@gmail.com>
2019-10-10 19:17:56 -0300

    Add click listener for item list

--- app/src/main/java/com/example/notekeeper/NoteListActivity.kt
+++ app/src/main/java/com/example/notekeeper/NoteListActivity.kt
@@ -1,6 +1,7 @@
 package com.example.notekeeper
 
 import android.os.Bundle
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.ListView
 import com.google.android.material.snackbar.Snackbar
@@ -31,6 +32,10 @@ class NoteListActivity : AppCompatActivity() {
             android.R.layout.simple_list_item_1, notes)
 
         listNotes.adapter = adapterNotes
+
+        listNotes.setOnItemClickListener{ parent, view, position, id ->
+            
+        }
     }
 
 }
